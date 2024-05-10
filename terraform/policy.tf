3d759c749ae3127699e8d44c22f846b4366c737d
Artem Ushenko <kingart030192kingart@gmail.com>
2024-05-10 13:23:08 +0300

    Update Terraform Files Configuration

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": "${aws_s3_bucket.bucket.arn}/*"
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.bucket.arn}/*"
       }
     ]
   })
